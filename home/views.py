88e0847f98bb025b76f50cab64114ff2545d783a
koladev <onaelmangabo@gmail.com>
2023-03-31 22:26:04 +0100

    :sparkles: add home views urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django import template
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+@login_required(login_url="/login/")
+def index(request):
+    context = {"segment": "index"}
+
+    html_template = loader.get_template("home/index.html")
+    return HttpResponse(html_template.render(context, request))
+
+
+@login_required(login_url="/login/")
+def pages(request):
+    context = {}
+
+    try:
+
+        load_template = request.path.split("/")[-1]
+        context["segment"] = load_template
+
+        html_template = loader.get_template("home/" + load_template)
+        return HttpResponse(html_template.render(context, request))
+
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template("home/page-404.html")
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+        html_template = loader.get_template("home/page-500.html")
+        return HttpResponse(html_template.render(context, request))
